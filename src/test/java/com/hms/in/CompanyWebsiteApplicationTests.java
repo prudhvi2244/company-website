935cf8e965a13ca8d54e4832b7453c1984c9f125
Mahadri <raj.rajeev2244@gmail.com>
2024-03-11 16:29:47 +0530

    First Commit

---
+++ src/test/java/com/hms/in/CompanyWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
